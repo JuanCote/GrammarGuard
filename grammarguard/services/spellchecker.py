201475107f7ba2d59d1bc00e6fc803fdd7639be4
JuanCote <lyy1707@gmail.com>
2024-01-23 16:58:26 +0200

    feat: highlighted text in input box

--- grammarguard/services/spellchecker.py
+++ grammarguard/services/spellchecker.py
@@ -5,8 +5,8 @@ def spelling_check(text: str) -> str:
     spell = SpellChecker()
     misspelled = spell.unknown(spell.split_words(text))
     highlighted_text = " ".join(
-        f'<span style="background-color: #FFCCCB;">{word}</span>'
-        if word in misspelled
+        f'<span class="bg-[#FFCCCB] text-black">{word}</span>'
+        if word.lower() in misspelled
         else word
         for word in text.split()
     )
