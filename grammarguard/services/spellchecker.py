b717085659c96812af49e63693a2716b31ffd49e
JuanCote <lyy1707@gmail.com>
2024-01-23 18:01:24 +0200

    feat: redesign analysis results

--- grammarguard/services/spellchecker.py
+++ grammarguard/services/spellchecker.py
@@ -1,3 +1,5 @@
+import string
+
 from spellchecker import SpellChecker
 
 
@@ -6,8 +8,13 @@ def spelling_check(text: str) -> str:
     misspelled = spell.unknown(spell.split_words(text))
     highlighted_text = " ".join(
         f'<span class="bg-[#FFCCCB] text-black">{word}</span>'
-        if word.lower() in misspelled
+        if remove_nonletters(word.lower()) in misspelled
         else word
         for word in text.split()
     )
     return highlighted_text
+
+
+def remove_nonletters(word: str) -> str:
+    translator = str.maketrans("", "", string.punctuation)
+    return word.translate(translator)
